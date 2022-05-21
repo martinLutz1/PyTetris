64ea1020157e911da5a101fe9c1211381ab1c3f3
Martin Lutz <martin_lutz4@yahoo.de>
2022-05-21 14:52:56 +0200

    Add figures

--- main.py
+++ main.py
@@ -1,7 +1,76 @@
-# Simple pygame program
-
-# Import and initialize the pygame library
+from enum import Enum
+from tkinter.tix import DirSelectBox
+from typing import List
 import pygame
+import numpy
+
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+class Position:
+    x: int
+    y: int
+
+
+class Figure:
+    position: Position
+    directions: list
+
+    def __init__(self, position, directions):
+        self.position = position
+        self.directions = directions
+
+
+class FigureBuilder:
+    class FigureType(Enum):
+        I = 0
+        O = 1
+        T = 2
+        S = 3
+        Z = 4
+        J = 5
+        L = 6
+
+    def new(position, figure_type) -> Figure:
+        match figure_type:
+            case FigureBuilder.FigureType.I:
+                return Figure(position, [Direction.DOWN, Direction.DOWN, Direction.DOWN])
+            case FigureBuilder.FigureType.O:
+                return Figure(position, [Direction.DOWN, Direction.RIGHT, Direction.UP])
+            case FigureBuilder.FigureType.I:
+                return Figure(position, [Direction.DOWN, Direction.DOWN, Direction.DOWN])
+            case FigureBuilder.FigureType.L:
+                return Figure(position, [Direction.DOWN, Direction.DOWN, Direction.RIGHT])
+
+
+def create_figure():
+    return Figure()
+
+
+class Tetris:
+    number_of_rows: int
+    number_of_columns: int
+    field: numpy.ndarray
+    figures: list
+
+    def _init_figures(self):
+        self.figures.append(Figure())
+
+    def __init__(self, number_of_rows, number_of_columns):
+        self.number_of_rows = number_of_rows
+        self.number_of_columns = number_of_columns
+        self.field = numpy.full((number_of_rows, number_of_columns), False)
+
+        self._init_figures()
+
+    def spawn_figure(self):
+
+        # Import and initialize the pygame library
 pygame.init()
 
 # Set up the drawing window
