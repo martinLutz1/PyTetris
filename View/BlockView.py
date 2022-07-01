martinLutz1/PyTetris

commit e9e983bfb9df83d9343927145eca041c1027d843

Cleanup TetrisView.

View/TetrisPainter.py
@@ -1,5 +1,4 @@
 from Figures import BlockColor, Figure
-from View.BackgroundView import BackgroundView
 from View.DrawSupport import *
 from View.ScoreView import ScoreView
 from View.TetrisInfoView import TetrisInfoView
@@ -11,9 +10,10 @@ import pygame
 
 
 class TetrisPainter:
+    background_color: pygame.Color = (200, 200, 200)
+
     screen: pygame.Surface
     view_description: ViewDescription
-    background_view: BackgroundView
     score_view: ScoreView
     tetris_view: TetrisView
     tetris_info_view: TetrisInfoView
@@ -24,23 +24,24 @@ class TetrisPainter:
         self.screen = pygame.display.set_mode(
             (screen_width, screen_height), pygame.FULLSCREEN)
 
-        tetris_view_width = screen_height / 2
-        tetris_view_x_offset = (screen_width - tetris_view_width) / 2
+        tetris_view_width = screen_height / 2 + 2 * TetrisView.border_thickness
+        tetris_view_x_offset = (
+            screen_width - tetris_view_width) / 2 - TetrisView.border_thickness
         self.view_description = ViewDescription(
-            screen_width, screen_height, tetris_view_width, tetris_view_x_offset)
+            screen_width, screen_height, tetris_view_width)
 
-        self.background_view = BackgroundView(
-            screen_width, screen_height, self.view_description)
-
+        # Init tetris view
         self.tetris_view = TetrisView(
-            tetris_view_width, screen_height, tetris_view_x_offset, 0,
-            number_of_rows, number_of_columns, number_of_offscreen_rows)
+            tetris_view_x_offset, 0, tetris_view_width, screen_height,
+            number_of_rows, number_of_columns, number_of_offscreen_rows, self.view_description)
 
+        # Init tetris info view
         tetris_info_view_x_offset = tetris_view_x_offset + tetris_view_width
         self.tetris_info_view = TetrisInfoView(
-            tetris_view_x_offset, screen_height, tetris_info_view_x_offset, 0, self.tetris_view.block_description)
+            tetris_info_view_x_offset, 0,
+            tetris_view_x_offset, screen_height, self.tetris_view.block_description)
 
-        self.background_view.draw(self.screen)
+        self.screen.fill(self.background_color)
 
     def add_to_static_blocks(self, figure: Figure):
         if figure is not None:
@@ -76,7 +77,7 @@ class TetrisPainter:
         pygame.display.update(updated_rects)
 
     def redraw_all(self, field: numpy.ndarray):
-        self.background_view.draw(self.screen)
+        self.screen.fill(self.background_color)
         self.tetris_info_view.draw_score(self.screen)
         self.tetris_info_view.draw_figure_preview(self.screen)
         self.tetris_view.set_static_blocks(field)

View/TetrisView.py
@@ -1,7 +1,7 @@
 
 from Figures import BlockColor, BlockPosition, Figure, Offset
 from View.FigureSprite import FigureSprite
-from View.ViewCommon import BlockDescription
+from View.ViewCommon import *
 from View.BlockView import TetrisBlockView
 
 from copy import deepcopy
@@ -10,30 +10,51 @@ import pygame
 
 
 class TetrisView:
+    border_thickness: int = 20
+
+    x_position: int
+    y_position: int
+    tetris_area_x_position: int
+    tetris_area_width: int
+    width: int
+    height: int
     number_of_rows: int
     number_of_columns: int
     number_of_offscreen_rows: int
     surface: pygame.Surface
-    block_description: BlockDescription
-    x_position: int
-    y_position: int
     figure_sprite: FigureSprite
-    static_block_views: list[TetrisBlockView] = []
-    background_color: pygame.Color = (50, 50, 50)
+    static_block_views: list[TetrisBlockView]
+    view_description: ViewDescription
+    block_description: BlockDescription
 
-    def __init__(self, width: int, height: int, x_position: int, y_position: int,
-                 number_of_rows: int, number_of_columns: int, number_of_offscreen_rows: int):
+    def __init__(self, x_position: int, y_position: int, width: int, height: int,
+                 number_of_rows: int, number_of_columns: int, number_of_offscreen_rows: int,
+                 view_description: ViewDescription):
+        self.x_position = x_position
+        self.y_position = y_position
+        self.tetris_area_x_position = x_position + self.border_thickness
+        self.tetris_area_width = width - 2 * self.border_thickness
+        self.width = width
+        self.height = height
         self.number_of_rows = number_of_rows
         self.number_of_columns = number_of_columns
         self.number_of_offscreen_rows = number_of_offscreen_rows
         self.surface = pygame.Surface((width, height))
-        block_width = width / number_of_columns
+        self.static_block_views = []
+        self.view_description = view_description
+
+        # Init block description
+        block_width = self.tetris_area_width / number_of_columns
         block_height = height / (number_of_rows - number_of_offscreen_rows)
         block_border_thickness = int(block_width / 8)
         self.block_description = BlockDescription(
             block_width, block_height, block_border_thickness)
-        self.x_position = x_position
-        self.y_position = y_position
+
+    def _draw_border(self, x_position: int):
+        border_rect = pygame.Rect(
+            x_position, self.y_position, self.border_thickness, self.view_description.screen_height)
+
+        pygame.draw.rect(self.surface, border_color, border_rect)
 
     def _get_block_view(self, color: pygame.Color, position: BlockPosition,
                         offset: Offset = Offset(0, 0)) -> TetrisBlockView:
@@ -57,16 +78,14 @@ class TetrisView:
         for block_view in block_views:
             if block_view.position.y >= -1:
                 drawn_rect = block_view.draw(self.surface)
-                drawn_rect.x += self.x_position
-                drawn_rect.y += self.y_position
                 updated_rects.append(drawn_rect)
 
-        return updated_rects
+        return self._to_absolute_position(updated_rects)
 
     def _to_absolute_position(self, rects: list[pygame.Rect]) -> list[pygame.Rect]:
         updated_rects = deepcopy(rects)
         for rect in updated_rects:
-            rect.x += self.x_position
+            rect.x += self.tetris_area_x_position
             rect.y += self.y_position
         return updated_rects
 
@@ -76,7 +95,7 @@ class TetrisView:
     def set_moving_figure(self, figure: Figure):
         self.figure_sprite = None
         self.figure_sprite = FigureSprite(
-            figure, self.block_description, self.number_of_offscreen_rows, self.background_color)
+            figure, self.block_description, self.number_of_offscreen_rows, tetris_bg_color, self.border_thickness)
 
     def update_figure_position(self, figure: Figure):
         self.figure_sprite.update_position(figure)
@@ -92,7 +111,8 @@ class TetrisView:
                     self.static_block_views.append(block_view)
 
     def blit(self, parent_surface: pygame.Surface):
-        parent_surface.blit(self.surface, (self.x_position, self.y_position))
+        parent_surface.blit(
+            self.surface, (self.tetris_area_x_position, self.y_position))
 
     def clear_moving_figure(self) -> list[pygame.Rect]:
         updated_rects = self.figure_sprite.clear(self.surface)
@@ -126,7 +146,9 @@ class TetrisView:
         return updated_rects
 
     def draw_all(self, parent_surface: pygame.surface):
-        self.surface.fill(self.background_color)
+        self.surface.fill(tetris_bg_color)
+        self._draw_border(0)
+        self._draw_border(self.border_thickness + self.tetris_area_width)
         self._draw_block_views(self.static_block_views)
         self.figure_sprite.draw(parent_surface)
         self.blit(parent_surface)

View/BackgroundView.py
@@ -1,42 +1,0 @@
-from View.ViewCommon import ViewDescription
-
-import pygame
-
-
-class BackgroundView:
-    color: pygame.Color = (50, 50, 50)
-    unused_area_color:  pygame.Color = (150, 150, 150)
-    border_color: pygame.Color = (40, 40, 40)
-    border_thickness: int = 20
-    view_description: ViewDescription
-    surface: pygame.Surface
-
-    def __init__(self, width: int, height: int, view_description: ViewDescription):
-        self.surface = pygame.Surface((width, height))
-        self.view_description = view_description
-
-    def _draw_border(self, x_position: int):
-        border_rect = pygame.Rect(
-            x_position, 0, self.border_thickness, self.view_description.screen_height)
-        pygame.draw.rect(self.surface, self.border_color, border_rect)
-
-    def _draw_unused_area(self, x_position: int):
-        rect_width = self.view_description.tetris_area_x_offset - self.border_thickness
-        border_rect = pygame.Rect(
-            x_position, 0, rect_width, self.view_description.screen_height)
-        pygame.draw.rect(self.surface, self.unused_area_color, border_rect)
-
-    def draw(self, parent_surface: pygame.Surface):
-        # Background color
-        self.surface.fill(self.color)
-        # Left area
-        self._draw_border(
-            self.view_description.tetris_area_x_offset - self.border_thickness)
-        self._draw_unused_area(0)
-        # Right area
-        self._draw_border(self.view_description.screen_width -
-                          self.view_description.tetris_area_x_offset)
-        self._draw_unused_area(self.view_description.screen_width -
-                               self.view_description.tetris_area_x_offset + self.border_thickness)
-
-        parent_surface.blit(self.surface, (0, 0))

View/DrawSupport.py
@@ -1,4 +1,4 @@
-from View.ViewCommon import BlockDescription
+from View.ViewCommon import *
 
 from copy import deepcopy
 import pygame
@@ -70,8 +70,5 @@ def draw_bordered_rounded_rect(surface, rect, color, border_color, corner_radius
 
 
 def draw_frame(surface: pygame.Surface, rect: pygame.Rect, block_description: BlockDescription):
-    frame_color = pygame.Color(100, 100, 100)
-    frame_border_color = pygame.Color(50, 50, 50)
-
     draw_bordered_rounded_rect(
-        surface, deepcopy(rect), frame_color, frame_border_color, 10, block_description.width / 4)
+        surface, deepcopy(rect), frame_color, border_color, 10, block_description.width / 4)

View/FigureSprite.py
@@ -9,10 +9,11 @@ import pygame
 
 class BlockSprite(pygame.sprite.Sprite):
     number_of_offscreen_rows: int
+    x_offset: int
 
     def __init__(self, description: BlockDescription, position: BlockPosition,
                  offset: Offset, color: BlockColor, background_color: pygame.Color,
-                 number_of_offscreen_rows: int):
+                 number_of_offscreen_rows: int, x_offset: int = 0):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface(
             (description.width, description.height))
@@ -21,10 +22,12 @@ class BlockSprite(pygame.sprite.Sprite):
         draw_bordered_rounded_rect(self.image, deepcopy(self.rect), color.body_color,
                                    color.border_color, 10, description.border_thickness)
         self.number_of_offscreen_rows = number_of_offscreen_rows
+        self.x_offset = x_offset
         self.update(position, offset)
 
     def update(self, block_position: BlockPosition, offset: Offset):
-        x_position = (float(block_position.x) + offset.x) * self.rect.width
+        x_position = (float(block_position.x) + offset.x) * \
+            self.rect.width + self.x_offset
         y_position = (float(block_position.y - self.number_of_offscreen_rows) + offset.y) * \
             self.rect.height
         self.rect.x = x_position
@@ -38,12 +41,12 @@ class FigureSprite():
     last_drawn_rects: list[pygame.Rect]
 
     def __init__(self, figure: Figure, block_description: BlockDescription,
-                 number_of_offscreen_rows: int, background_color: pygame.Color):
+                 number_of_offscreen_rows: int, background_color: pygame.Color, x_offset: int = 0):
         block_sprites = []
         for block_position in figure.get_blocks():
             block_sprite = BlockSprite(
                 block_description, block_position, figure.offset,
-                figure.block_color, background_color, number_of_offscreen_rows)
+                figure.block_color, background_color, number_of_offscreen_rows, x_offset)
             block_sprites.append(block_sprite)
 
         self.block_sprite_group = pygame.sprite.RenderUpdates(block_sprites)

View/TetrisInfoView.py
@@ -17,7 +17,7 @@ class TetrisInfoView:
     x_position: int
     y_position: int
 
-    def __init__(self, width: int, height: int, x_position: int, y_position: int, block_description: BlockDescription):
+    def __init__(self, x_position: int, y_position: int, width: int, height: int,  block_description: BlockDescription):
         self.surface = pygame.Surface((width, height))
         self.x_position = x_position
         self.y_position = y_position

View/ViewCommon.py
@@ -19,13 +19,11 @@ class ViewDescription:
     screen_width: int
     screen_height: int
     tetris_width: int
-    tetris_area_x_offset: int
 
-    def __init__(self, screen_width: int, screen_height: int, tetris_width: int, x_screen_offset: int):
+    def __init__(self, screen_width: int, screen_height: int, tetris_width: int):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.tetris_width = tetris_width
-        self.tetris_area_x_offset: int = x_screen_offset
 
 
 _font_path: str = "Font/OpenDyslexic3-Regular.ttf"
@@ -36,3 +34,6 @@ medium_font_height: int = 80
 big_font = pygame.font.Font(_font_path, 50)
 big_font_height: int = 120
 text_color = pygame.Color(30, 30, 30)
+tetris_bg_color = pygame.Color(80, 80, 80)
+border_color = pygame.Color(50, 50, 50)
+frame_color = pygame.Color(100, 100, 100)

commit 1fca6b3701daa098993f8344e8e094fd3f783b67

Fix tetris blocks moving position after scoring.
Fixes: e9e983bfb9df83d9343927145eca041c1027d843

View/BlockView.py
@@ -33,14 +33,16 @@ class TetrisBlockView(BlockView):
     position: BlockPosition
     offset: Offset
     color: BlockColor
+    x_offset: int
 
     def __init__(self, block_description: BlockDescription, position: BlockPosition,
-                 offset: Offset, color: BlockColor):
+                 offset: Offset, color: BlockColor, x_offset: int = 0):
         super().__init__(0, 0, block_description, color)
         self.block_description = block_description
         self.position = position
         self.offset = offset
         self.color = color
+        self.x_offset = x_offset
 
     def update_position(self, position: BlockPosition, offset: Offset):
         self.position = deepcopy(position)
@@ -48,7 +50,7 @@ class TetrisBlockView(BlockView):
 
     def draw(self, parent_surface: pygame.Surface) -> pygame.Rect:
         self.x_position = (float(self.position.x) + self.offset.x) * \
-            self.block_description.width
+            self.block_description.width + self.x_offset
         self.y_position = (float(self.position.y) + self.offset.y) * \
             self.block_description.height
         return super().draw(parent_surface)

View/TetrisPainter.py
@@ -41,8 +41,6 @@ class TetrisPainter:
             tetris_info_view_x_offset, 0,
             tetris_view_x_offset, screen_height, self.tetris_view.block_description)
 
-        self.screen.fill(self.background_color)
-
     def add_to_static_blocks(self, figure: Figure):
         if figure is not None:
             self.tetris_view.add_to_static_blocks(figure)

View/TetrisView.py
@@ -57,10 +57,10 @@ class TetrisView:
         pygame.draw.rect(self.surface, border_color, border_rect)
 
     def _get_block_view(self, color: pygame.Color, position: BlockPosition,
-                        offset: Offset = Offset(0, 0)) -> TetrisBlockView:
+                        offset: Offset = Offset(0, 0), x_offset: int = 0) -> TetrisBlockView:
         block_position = BlockPosition(
             position.x, position.y - self.number_of_offscreen_rows)
-        return TetrisBlockView(self.block_description, block_position, offset, color)
+        return TetrisBlockView(self.block_description, block_position, offset, color, self.border_thickness)
 
     def _get_block_views(self, figure: Figure) -> list[TetrisBlockView]:
         block_views = []